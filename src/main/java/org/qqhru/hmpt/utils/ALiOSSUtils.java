1e229a7fea0c105b7085861a1dbc456ec31f6752
Droit <111848491+Right202209@users.noreply.github.com>
2025-03-13 20:50:25 +0800

    Create ALiOSSUtils.java

---
+++ src/main/java/org/qqhru/hmpt/utils/ALiOSSUtils.java
@@ -0,0 +1,45 @@
+package org.qqhru.hmpt.utils;
+
+import com.aliyun.oss.OSS;
+import com.aliyun.oss.OSSClientBuilder;
+import org.springframework.stereotype.Component;
+import org.springframework.web.multipart.MultipartFile;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.UUID;
+
+@Component
+public class AliOSSUtils {
+    private String endpoint = "https://oss-cn-beijing.aliyuncs.com";
+    private String accessKeyId = "**";
+    private String accessKeySecret = "***";
+    private String bucketName = "****";
+
+    /**
+     * 实现上传图片到OSS
+     */
+    public String upload(MultipartFile multipartFile) throws IOException {
+        // 获取上传的文件的输入流
+        InputStream inputStream = multipartFile.getInputStream();
+
+        // 避免文件覆盖
+        SimpleDateFormat simpleDateFormat = new SimpleDateFormat("yyyy/MM/dd");
+        String format = simpleDateFormat.format(new Date());
+        String originalFilename = multipartFile.getOriginalFilename();
+        String fileName =format+ "/"+ System.currentTimeMillis() + originalFilename.substring(originalFilename.lastIndexOf("."));
+
+        //上传文件到 OSS
+        OSS ossClient = new OSSClientBuilder().build(endpoint, accessKeyId, accessKeySecret);
+        ossClient.putObject(bucketName, fileName, inputStream);
+
+        //文件访问路径
+        String url = endpoint.split("//")[0] + "//" + bucketName + "." + endpoint.split("//")[1] + "/" + fileName;
+
+        // 关闭ossClient
+        ossClient.shutdown();
+        return url;// 把上传到oss的路径返回
+    }
+}
